5d2c4d73b6ca5c51992b065969741a78eee3b1b4
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:33:17 +0200

    Initial commit

---
+++ CS002SoucetCisel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CS002SoucetCisel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Soucet(15,17));
+            Console.ReadKey();
+        }
+        static int Soucet(int A, int B)
+        {
+            return A + B;
+        }
+    }
+}
